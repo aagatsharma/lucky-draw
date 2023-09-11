5fb5fb7a1c534e13c41d431e596cc3ad921aa508
aagatsharma <aagats72@gmail.com>
2023-09-11 14:59:08 +0545

    Carousel added

--- src/pages/BuyNow.jsx
+++ src/pages/BuyNow.jsx
@@ -1,26 +1,44 @@
+import { useState } from "react";
+
 function BuyNow() {
-  const buttons = [];
+  const [selectedButtons, setSelectedButtons] = useState([]);
 
-  for (let i = 1; i <= 100; i++) {
-    //buttons
-    buttons.push(
-      <button key={i} className="btn btn-circle btn-outline">
-        <p>{i}</p>
-      </button>
-    );
-  }
-
+  const handleButtonClick = (buttonIndex) => {
+    if (selectedButtons.includes(buttonIndex)) {
+      setSelectedButtons(
+        selectedButtons.filter((index) => index !== buttonIndex)
+      );
+    } else {
+      setSelectedButtons([...selectedButtons, buttonIndex]);
+    }
+  };
   return (
     <div className="min-h-screen bg-base-200 pt-16">
-      <div className="text-center">
-        <div>
-          <h1 className="text-5xl font-bold">Choose numbers</h1>
-          <div className="max-w-xl m-auto my-10">
-            <div className="grid grid-cols-10 max-sm:grid-cols-5 gap-5 px-5">
-              {buttons}
-            </div>
+      <div>
+        <h1 className="text-5xl font-bold text-center">Choose numbers</h1>
+        <div className="max-w-xl m-auto my-10">
+          <div className="grid grid-cols-10 max-sm:grid-cols-5 gap-5 px-5">
+            {[...Array(100)].map((x, i) => (
+              <button
+                key={i}
+                className={`btn btn-circle btn-outline ${
+                  selectedButtons.includes(i)
+                    ? "bg-red-500 text-white border-red-500"
+                    : ""
+                }`}
+                onClick={() => handleButtonClick(i)}
+              >
+                <p>{i + 1}</p>
+              </button>
+            ))}
           </div>
-          <button className="btn btn-primary">Buy Now</button>
+          <div className="join grid grid-cols-2 my-6">
+            <button className="join-item btn btn-outline">Previous</button>
+            <button className="join-item btn btn-outline">Next</button>
+          </div>
+          <div className="flex justify-center my-6">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
         </div>
       </div>
     </div>

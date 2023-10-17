e0de619b20258062616440c382ae61605a7be87a
Sukrajchaudhary <sukrajchaudhary90@gmail.com>
2023-10-17 10:09:47 +0545

    last

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -3,15 +3,16 @@ import video from "../assets/v.mp4";
 
 const Video = () => {
   return (
-    <div className="hero min-h-screen bg-base-200 flex justify-center items-center">
-      <video
-        src={video}
-        width="640px"
-        height="320px"
-        controls // Add 'controls' to display video controls
-        allowFullScreen
-        autoPlay
-/>
+    <div className="hero min-h-screen bg-base-200 flex justify-center items-center p-2" >
+      <iframe
+        width="560"
+        height="400"
+        src="https://www.youtube.com/embed/WqKbbXseJ3c?si=W6meL5hOU95Q49e3"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
